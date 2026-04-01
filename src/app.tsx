c159b3836e680979b9afd59511eda397e93310e1
nrjseah-nex0 <nrjseah00@gmail.com>
2026-04-02 01:04:19 +0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,40 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DefaultProviders } from "./components/providers/default.tsx";
-import AuthCallback from "./pages/auth/Callback.tsx";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import DashboardLayout from "./layouts/DashboardLayout.tsx";
-import DashboardPage from "./pages/dashboard/page.tsx";
-import { useServiceWorker } from "@/hooks/use-service-worker.ts";
-
-function AppRoutes() {
-  useServiceWorker();
-  return (
-    <DefaultProviders>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
-
-          {/* Dashboard routes */}
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
-
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </DefaultProviders>
-  );
-}
-import Background from "./components/Background";
-
 export default function App() {
   return (
-    <>
-      <Background />
-      {/* باقي الموقع */}
-    </>
+    <div>
+      <Navbar />
+      <Hero />
+      <Features />
+    </div>
   );
 }
+
+
