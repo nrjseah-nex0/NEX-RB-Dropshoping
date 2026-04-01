230da9fa2c6a8d0c238168dc43b7dc358706b52b
nrjseah-nex0 <nrjseah00@gmail.com>
2026-04-02 01:50:04 +0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Background from "./components/Background";
+
 export default function App() {
   return (
-    <div>
+    <div style={{ fontFamily: "sans-serif" }}>
+      <Background />
       <Navbar />
       <Hero />
       <Features />
     </div>
   );
 }
-
-
